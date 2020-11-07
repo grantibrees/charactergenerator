bb5cbdd2c876550ba3a4590ce161d7ec1c8fe53f
grantibrees <grantibrees@gmail.com>
2020-11-07 15:32:18 -0700

    Added setup for name and user

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <img src="../assets/img/cw-circle.png" alt="CodeWorks Logo">
-    <h1>Welcome {{ name }}</h1>
-  </div>
+<div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+  <img src="../assets/img/cw-circle.png" alt="CodeWorks Logo">
+  <h1>Welcome {{ user.name }}</h1>
+</div>
 </template>
 
 <script>
+import {
+  computed
+} from "vue"
+import {
+  AppState
+} from "../AppState"
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+    return {
+      user: computed(() => AppState.user)
+    }
+  }
 }
 </script>
 
-<style scoped lang="scss">
-.home{
+<style lang="scss" scoped>
+.home {
   text-align: center;
-  img{
+
+  img {
     height: 250px;
     width: 250px;
   }
